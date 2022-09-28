6c79ee719d6c636aa27977d3b8997f52ca08dc52
Jesus Barrientos <jabarri1@gmail.com>
2022-09-28 15:16:24 -0700

    first upload

---
+++ data.js
@@ -0,0 +1,101 @@
+leafData = [
+    {
+      ID: "til_floterracity",
+      Name: "Floterra City",
+      Description: "The capital city of Floterra",
+      Type: "Major City",
+      Continent: "Floterra",
+      X: "-31.503629",
+      Y: "-19.731445"
+    },
+    {
+      ID: "til_motreg",
+      Name: "Motreg",
+      Description: "A small town on the west side of Floterra",
+      Type: "Town",
+      Continent: "Floterra",
+      X: "-28.149503",
+      Y: "-24.082031"
+    },
+    {
+      ID: "til_lumeroth",
+      Name: "Lumeroth",
+      Description: "A town midway from the city to Flamari Castle",
+      Type: "Town",
+      Continent: "Floterra",
+      X: "-28.844674",
+      Y: "-14.0625"
+    },
+    {
+      ID: "til_thegrove",
+      Name: "Floterra Grove",
+      Description: "A grove :)",
+      Type: "Landmark",
+      Continent: "Floterra",
+      X: "-29.496988",
+      Y: "-11.425781"
+    },
+    {
+      ID: "til_zephyr",
+      Name: "Zephyr Tower",
+      Description: "Tower of the :)",
+      Type: "Wizard Tower",
+      Continent: "Floterra",
+      X: "-33.504759",
+      Y: "-27.333984"
+    },
+    {
+      ID: "til_trident",
+      Name: "The Trident",
+      Description: "Tower of the :)",
+      Type: "Wizard Tower",
+      Continent: "Floterra",
+      X: "-32.842674",
+      Y: "-9.580078"
+    },
+    {
+      ID: "til_satar",
+      Name: "Satar Castle",
+      Description: "Castle offfffff ??? :)",
+      Type: "Castle",
+      Continent: "Floterra",
+      X: "-25.958045",
+      Y: "-18.28125"
+    },
+    {
+      ID: "til_highplains",
+      Name: "High Plainstower",
+      Description: "Tower of the :)",
+      Type: "Wizard Tower",
+      Continent: "Floterra",
+      X: "-20.262197",
+      Y: "-26.05957"
+    },
+    {
+      ID: "til_flametower",
+      Name: "Tower of Flame",
+      Description: "Tower of the :)",
+      Type: "Wizard Tower",
+      Continent: "Floterra",
+      X: "-20.879343",
+      Y: "-7.866211"
+    },
+    {
+      ID: "til_flamar",
+      Name: "Flamar Castle",
+      Description: "Castle and home of worship",
+      Type: "Castle",
+      Continent: "Floterra",
+      X: "-20.879343",
+      Y: "-7866211"
+    },
+    {
+      ID: "til_zatar",
+      Name: "Zatar",
+      Description: "A village :)",
+      Type: "Town",
+      Continent: "Floterra",
+      X: "-21.493964",
+      Y: "-18.369141"
+    }
+  ]
